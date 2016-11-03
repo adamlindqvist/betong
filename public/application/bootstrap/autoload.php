46c0e5405ff24abe2c272d9ee920d4d1129fbfba
Adam Lindqvist <adam@wmyl.se>
2016-11-03 21:28:40 +0100

    remove dotenv

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -8,13 +8,6 @@
 
 require_once(DIR_BASE . '/../vendor/autoload.php');
 
-try {
-    $dotenv = new \Dotenv\Dotenv(DIR_BASE . '/../');
-    $dotenv->load();
-} catch (Exception $e) {
-    exit('Missing the environment configuration');
-}
-
 if (!@include(DIR_BASE_CORE . '/' . DIRNAME_VENDOR . '/autoload.php')) {
     die('Third party libraries not installed. Make sure that composer has required libraries in the concrete/ directory.');
 }
